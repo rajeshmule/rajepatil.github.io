7e86af1f19ce1476931e71f903081c4e2bc26839
Rajesh mule <rajeshsmule@gmail.com>
2020-03-10 17:25:53 +0530

    Updates

---
+++ precache-manifest.fdf55d70b1a19eb6b83b52312744083f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9e23fd2473bb64c34c942bf514de96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7421440c5a395515e8d1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb70f858f29b3d6a9dd8",
+    "url": "/static/js/2.b7cee624.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b7cee624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7421440c5a395515e8d1",
+    "url": "/static/js/main.a83f5d0e.chunk.js"
+  },
+  {
+    "revision": "2f452ef6ea591db05390",
+    "url": "/static/js/runtime-main.f74b3552.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
